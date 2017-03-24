eff722e92950096128f673463452c10c25e4db96
James R. Harvey <WyomingGeezer@gmail.com>
2017-03-24 00:45:37 -0600

    Add files via upload

--- IambicKeyer.ino
+++ IambicKeyer.ino
@@ -19,6 +19,21 @@
 2. Add code for recording Morse code as sent.
 3. Flesh out documentation, including a schematic of the desired implementation.
 '****************************************************************
+    Copyright (C) 2017 JAMES R. HARVEY
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 */
 
 
@@ -111,6 +126,7 @@ void setup() {
   setWordsPerMinute(18);
   Serial.begin(9600);
   Serial.println(Copyright);
+  Serial.println("Type $CR for additional copyright information.");
 
   delay(1000);
 
@@ -455,7 +471,10 @@ String convertToMorse(String strText) {
 // END OF TEXT STREAM MORSE CODE HANDLING
 /////////////////////////////////////////////////////////////////
 
-
+// TODO: 03/24/2017 This code works in Arduino serial monitor but not in a standard
+// terminal program. The code should not exit the routine UNTIL a CR is received. Better yet,
+// the code should simply gather characters in a non-blocking manner and report when a full
+// line has been received.
 String getInputLine(void) {
 
   String strInput;
@@ -527,6 +546,15 @@ boolean processCommands(String strCmd) {
     return true;
   }
 
+
+  if (strCmd.substring(0, 3) == "$CR") {
+
+
+    Serial.println("Copyright information PENDING");
+    Serial.println("See <http://www.gnu.org/licenses/>");
+    
+    return true;
+  }
 
 
   if (strCmd.substring(0, 6) == "$RESET") {
